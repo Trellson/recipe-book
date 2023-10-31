04b9343dc9454682c061cc2687657137dec4d156
Trellson <trellson13@gmail.com>
2023-10-31 18:08:32 -0400

    initial commit

---
+++ recipe.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css">
+
+    <title>Recipe book</title>
+  </head>
+  <body>
+    <div class="container">
+      
+    </div>
+
+   
+  </body>
+</html>
