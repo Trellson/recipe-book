bb404bc827d018d00efc1ee1011aecb69f53e913
Trellson <trellson13@gmail.com>
2023-10-31 18:23:27 -0400

    set up recipe form

--- recipe.php
+++ recipe.php
@@ -12,7 +12,42 @@
   </head>
   <body>
     <div class="container">
-      
+    <form method="post">
+  <div class="form-group">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" name="title" placeholder="Recipe Title" required>
+  </div>
+  <div class="form-group">
+    <label for="description">Description</label>
+    <textarea class="form-control" id="description" name="description" placeholder="Recipe Description" rows="3" required></textarea>
+  </div>
+  <div class="form-group">
+    <label for="ingredients">Ingredients</label>
+    <textarea class="form-control" id="ingredients" name="ingredients" placeholder="Ingredients List" rows="5" required></textarea>
+  </div>
+  <div class="form-group">
+    <label for="instructions">Instructions</label>
+    <textarea class="form-control" id="instructions" name="instructions" placeholder="Recipe Instructions" rows="5" required></textarea>
+  </div>
+  <div class="form-group">
+    <label for="category">Category</label>
+    <input type="text" class="form-control" id="category" name="category" placeholder="Recipe Category" required>
+  </div>
+  <div class="form-group">
+    <label for="preparationTime">Preparation Time (minutes)</label>
+    <input type="number" class="form-control" id="preparationTime" name="preparationTime" placeholder="Preparation Time" required>
+  </div>
+  <div class="form-group">
+    <label for="cookTime">Cook Time (minutes)</label>
+    <input type="number" class="form-control" id="cookTime" name="cookTime" placeholder="Cook Time" required>
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="favorite" name="favorite">
+    <label class="form-check-label" for="favorite">Add to Favorites</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
     </div>
 
    
